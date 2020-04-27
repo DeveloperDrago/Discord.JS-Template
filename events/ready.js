c877f3b2eafb8b784a33133b8692e966ff006046
Jason <yadiel.diazjimenez@outlook.com>
2020-04-27 18:08:23 -0500

    Change ready event

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 module.exports = (client) => {
   console.log(`Bot Successfully Initallized`);
   //When the bot is online it'll log that it's On
-  client.user.setActivity(`This Prefix: >`);
+  client.user.setActivity(`The Default Prefix: >`);
+  //this will change what game the bot is playing
 }
